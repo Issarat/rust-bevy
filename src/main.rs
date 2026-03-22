1e52c6b75a215bc252649dc2bb945dc34bb7919e
Issarat.j <159237803+Issarat@users.noreply.github.com>
2026-03-23 01:17:54 +0700

    add :  player sprites

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,27 @@
+use bevy::prelude::*;
 
 fn main() {
-    println!("Hello World")
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, (spawn_player, spawn_camera))
+        .run();
 }
+#[derive(Component)] 
+pub struct Player;   
+
+pub fn spawn_player(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>, 
+) {
+    commands.spawn((
+        Sprite::from_image(asset_server.load("sprites/ball_blue_large.png")),
+        Transform::from_xyz(0.0, 0.0, 0.0), 
+        Player,
+    ));
+}
+
+pub fn spawn_camera(mut commands: Commands) {
+    commands.spawn(Camera2d::default());
+}
+
 
